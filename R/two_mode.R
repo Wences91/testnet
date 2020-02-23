ec20437573f67376170c67a7c5a0e72267a7235a
Wences91 <wences91@gmail.com>
2020-02-23 19:41:15 +0100

    Update two_mode.R

--- R/two_mode.R
+++ R/two_mode.R
@@ -1,18 +1,22 @@
 #' two_mode
 #' 
 #' @param edges edges data.frame
+#' @param size node sizes
+#' @param degree_mode which degree calculates (in, out, total)
 #' @export
 #' @importFrom igraph graph_from_data_frame degree cluster_louvain layout_nicely as.undirected V E "V<-"
 #' 
 
-two_mode <- function(edges){
+two_mode <- function(edges, size=1, degree_mode='in'){
   # create two-mode network
-  g <- igraph::graph_from_data_frame(edges, directed = TRUE)
+  g <- igraph::graph_from_data_frame(edges, directed=TRUE)
   g <- giant_component(g)
   
-  g$indegree <- igraph::degree(g, mode = 'in')
+  g$indegree <- igraph::degree(g, mode = degree_mode)
+  g$nsize <- size * (g$degree/sum(g$degree))
   
   communities <- igraph::cluster_louvain(igraph::as.undirected(g))
+  cat(communities$modularity)
   
   igraph::V(g)$colors <- ifelse(substr(igraph::V(g)$name, 1, 2) == 'S-', '#add8e6', '#da86c5')
   
