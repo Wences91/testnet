c6564e3d84b4545aed675967d3e11e5ac5b7f335
Wences91 <wences91@gmail.com>
2020-02-24 23:31:32 +0100

    Update fails_dist.R

--- R/fails_dist.R
+++ R/fails_dist.R
@@ -8,13 +8,13 @@
 
 fails_dist <- function(edges, by='questions'){
   if(by=='questions'){
-    ggplot2::ggplot(data=edges, ggplot2::aes(reorder(source, -weight, sum), weight)) +
+    ggplot2::ggplot(data=edges, ggplot2::aes(reorder(target, -weight, sum), weight)) +
       ggplot2::geom_col(fill='#72bcd4') +
       ggplot2::labs(x='Answer', y='Count') +
       ggplot2::theme_minimal() +
       ggplot2::theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
   }else if(by=='students'){
-    ggplot2::ggplot(data=edges, ggplot2::aes(reorder(target, -weight, sum), weight)) +
+    ggplot2::ggplot(data=edges, ggplot2::aes(reorder(source, -weight, sum), weight)) +
       ggplot2::geom_col(fill='#d4728b') +
       ggplot2::labs(x='Student', y='Count') +
       ggplot2::theme_minimal() +
